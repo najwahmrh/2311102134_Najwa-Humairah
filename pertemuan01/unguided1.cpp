2776782ebcfb990503fe8cbc99a48999a02e9d1d
Najwa humairah <humairahnajwa3@gmail.com>
2024-03-12 22:40:42 +0700

    Laprak1_Pertemuan1

--- pertemuan01/unguided1.cpp
+++ pertemuan01/unguided1.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
-#include <iomanip>
+#include <cmath>
 
+using namespace std;
+
+float hitungLuasPersegi_134(int sisi_134){
+    return sisi_134*sisi_134;
+}
+
+float hitungVolumeKubus_134(int sisi_134){
+    return sisi_134*sisi_134*sisi_134;
+}
+
+int main(){
+    //Mendefenisikan variabel dengan menggunakan tipe data int
+    int panjangSisi;
+
+    cout << "Masukkan panjang sisi persegi :";
+    cin >> panjangSisi;
+
+    float luasPersegi = hitungLuasPersegi_134(panjangSisi);
+    cout << "luas persegi dengan sisi " << panjangSisi << "adalah :" << luasPersegi << endl;
+    float volumeKubus = hitungVolumeKubus_134(panjangSisi);
+    cout << "Volume kubus dengan sisi " << panjangSisi << "adalah :" << volumeKubus << endl;
+
+    return 0;
+
+}
